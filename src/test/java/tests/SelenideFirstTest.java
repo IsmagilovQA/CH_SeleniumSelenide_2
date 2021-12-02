64ea5fff7eac20c0c8f6aed9ee0f19da0fe575ae
Vitalii Ismagilov <ismagilov.vel@gmail.com>
2021-12-02 13:00:14 +0200

    Add first simple test

---
+++ src/test/java/tests/SelenideFirstTest.java
@@ -0,0 +1,17 @@
+package tests;
+
+import com.codeborne.selenide.Condition;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selenide.*;
+
+public class SelenideFirstTest {
+
+    @Test
+    public void test() {
+        open("https://www.google.com/");
+        $x("//input[@name='q']").setValue("ChromeDriver").pressEnter();
+        $("#result-stats").shouldBe(visible);
+    }
+}
